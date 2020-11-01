5ba90fafe9f7da1c5c46cc04ec5a18f3898b787d
wilber carlo <wilbercarlo87@gmail.com>
2020-11-01 16:50:19 -0400

    instalando npm express-fileupload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page..*/
+/* GET home page...*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RECUPERATORIO SEMINARIO DE SISTEMAS' });
+  res.render('index', { title: 'RECUPERATORIO DE SEMINARIO DE SISTEMAS' });
 });
 
 module.exports = router;
